7d839d0cd155f26fd163df819ce0c15e437f7b31
dkasabovn <dkasabovn@gmail.com>
2022-01-30 01:28:33 -0600

    gabo update 3

--- magefile.go
+++ magefile.go
@@ -37,11 +37,23 @@ func (Build) Scraper() error {
 	return sh.RunV("docker", "build", "--tag", "polx/scraper", "--file", "./app/cmd/scraper/Dockerfile", ".")
 }
 
+func (Build) Analytics() error {
+	mg.Deps(Build.AllImages)
+	return sh.RunV("docker", "build", "--tag", "polx/analytics", "--file", "./app/cmd/analytics/Dockerfile", ".")
+}
+
 func (Run) Scraper() error {
 	mg.Deps(Build.Scraper)
 	sh.RunV("docker", "container", "stop", "polx_scraper")
 	sh.RunV("docker", "container", "rm", "polx_scraper")
-	return sh.RunV("docker", "run", "-p", "6969:6969", "--name=polx_scraper", "polx/scraper")
+	return sh.RunV("docker", "run", "-d", "--name=polx_scraper", "--network=host", "polx/scraper")
+}
+
+func (Run) Analytics() error {
+	mg.Deps(Build.Analytics)
+	sh.RunV("docker", "container", "stop", "polx_analytics")
+	sh.RunV("docker", "container", "rm", "polx_analytics")
+	return sh.RunV("docker", "run", "-p", "6969:6969", "-d", "--name=polx_analytics", "--network=host", "polx/analytics")
 }
 
 func (Run) ScraperDB() error {
@@ -57,4 +69,13 @@ func (Run) ScraperDB() error {
 	err = sh.RunV("docker", "run", "-p", "5432:5432", "-d", "--name=scraper_db", "--network=host", "polx/scraperdb")
 
 	return err
+}
+
+func (Run) TheEntireApp() error {
+	mg.Deps(
+		Run.ScraperDB,
+		Run.Scraper,
+		Run.Analytics,
+	)
+	return nil
 }
